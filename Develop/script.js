bfee41a66e509990d181e2ce20e230b6061bef70
Lisheng <lishengyu1986@gmail.com>
2020-03-23 21:38:20 +0800

    create script.js

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+//using Umbrella JS
+// can't find .ready() method
+
+var apiKey = "";
+
+function init() {
+    //do nothing if localstorage is null
+    //get data from localstorage saved history
+    //put into preset buttons
+}
+
+//random button
+u("#random-button").on("click", function(event) {
+    //ajax call query
+    //display data in cards
+})
+
+u("#preset").on("click", function(event) {
+    
+})
