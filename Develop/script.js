3b104e42af65d391a1e94b3b2320b63bf823cded
Lisheng <lishengyu1986@gmail.com>
2020-03-29 16:23:27 +0800

    document ready scope fix

--- Develop/script.js
+++ Develop/script.js
@@ -152,149 +152,149 @@ $(document).ready(function() {
 
     getDrinkInstructions(drinkResponse);
   }
+
+  function getDrinkInstructions(drinkResponse) {
+    var instruction = drinkResponse.drinks[0].strInstructions;
+    console.log(instruction);
+
+    instructionHeader = $("<p>").text("Instructions:");
+    instructionText = $("<p>").text(instruction);
+
+    instructionHeader.append(instructionText);
+    $("#ingredientHeader").append(instructionHeader);
+  }
+
+  function getRecipe() {
+    $("#recipeSummary").empty();
+
+    var recipeUrl =
+      "https://api.spoonacular.com/recipes/random?apiKey=7c0986ad6c3445a491cf76f7d2a655ab";
+
+    $.ajax({
+      url: recipeUrl,
+      method: "GET"
+    }).then(function(recipeResponse) {
+      console.log(recipeResponse.recipes[0]);
+      var recipeImage = recipeResponse.recipes[0].image;
+      var recipeName = recipeResponse.recipes[0].title;
+      var recipeSummary = recipeResponse.recipes[0].summary;
+
+      summaryHTML = $("<p>").text(recipeSummary);
+
+      $("#recipeImage").css("background-image", "url(" + recipeImage + ")");
+      $("#recipeName").text(recipeName);
+      $("#recipeSummary").append(recipeSummary);
+
+      //render recipe history buttons
+      var newRecipe = $("<button>");
+      newRecipe.addClass("button recipe-button").text(recipeName);
+      $("#recipe-history").append(newRecipe);
+      $(".recipe-button")
+        .first()
+        .remove();
+      recipeList.shift();
+      var recipeEL = {
+        reciImg: recipeImage,
+        reciNam: recipeName,
+        reciSum: recipeSummary
+      };
+      recipeList.push(recipeEL);
+      localStorage.setItem("recipeHistory", JSON.stringify(recipeList));
+    });
+  }
+
+  function getMovie() {
+    $("#movieInfo").empty();
+
+    var movieUrl =
+      "https://api.themoviedb.org/3/discover/movie?api_key=f239018644aca27fb1793b1dbcab8d4c";
+
+    $.ajax({
+      url: movieUrl,
+      method: "GET"
+    }).then(function(movieResponse) {
+      var movieID = Math.floor(Math.random() * 19);
+      console.log(movieResponse.results[movieID]);
+      var movie = movieResponse.results[movieID].title;
+      var poster =
+        "https://image.tmdb.org/t/p/original" +
+        movieResponse.results[movieID].poster_path;
+      var rating = movieResponse.results[movieID].vote_average + "/10";
+      var plot = movieResponse.results[movieID].overview;
+
+      $("#movieName").text(movie);
+
+      $("#movieImage").css("background-image", "url(" + poster + ")");
+
+      ratingHeader = $("<p>").text("Rating:");
+      ratingText = $("<p>").text(rating);
+      plotHeader = $("<p>").text("Plot:");
+      plotText = $("<p>").text(plot);
+      breakHTML = $("<br>");
+
+      $("#movieInfo").append(
+        ratingHeader,
+        ratingText,
+        breakHTML,
+        plotHeader,
+        plotText
+      );
+
+      //render movie history buttons
+      var newMovie = $("<button>");
+      newMovie.addClass("button movie-button").text(movie);
+      $("#movie-history").append(newMovie);
+      $(".movie-button")
+        .first()
+        .remove();
+      movieList.shift();
+      var movieEl = {
+        movNam: movie,
+        movImg: poster,
+        movRate: rating,
+        movPlot: plot
+      };
+      movieList.push(movieEl);
+      localStorage.setItem("movieHistory", JSON.stringify(movieList));
+    });
+  }
+
+  function init() {
+    //keep last three random history
+    var drinkHistory = JSON.parse(localStorage.getItem("drinkHistory"));
+    if (drinkHistory == null) {
+      localStorage.setItem("drinkHistory", JSON.stringify(drinkList));
+      for (var i = 0; i < drinkList.length; i++) {
+        $("#drink-" + i).text(drinkList[i]);
+      }
+    } //handle first time use
+    drinkList = drinkHistory;
+    for (var i = 0; i < drinkHistory.length; i++) {
+      $("#drink-" + i).text(drinkHistory[i].drinkNam);
+    }
+
+    var recipeHistory = JSON.parse(localStorage.getItem("recipeHistory"));
+    if (recipeHistory == null) {
+      localStorage.setItem("recipeHistory", JSON.stringify(recipeList));
+      for (var i = 0; i < recipeList.length; i++) {
+        $("#recipe-" + i).text(recipeList[i]);
+      }
+    } //handle first time use
+    recipeList = recipeHistory;
+    for (var i = 0; i < recipeHistory.length; i++) {
+      $("#recipe-" + i).text(recipeHistory[i].reciNam);
+    }
+
+    var movHistory = JSON.parse(localStorage.getItem("movieHistory"));
+    if (movHistory == null) {
+      localStorage.setItem("movieHistory", JSON.stringify(movieList));
+      for (var i = 0; i < movieList.length; i++) {
+        $("#movie-" + i).text(movieList[i]);
+      }
+    } //handle first time use
+    movieList = movHistory;
+    for (var i = 0; i < movHistory.length; i++) {
+      $("#movie-" + i).text(movHistory[i].movNam);
+    }
+  }
 });
-
-function getDrinkInstructions(drinkResponse) {
-  var instruction = drinkResponse.drinks[0].strInstructions;
-  console.log(instruction);
-
-  instructionHeader = $("<p>").text("Instructions:");
-  instructionText = $("<p>").text(instruction);
-
-  instructionHeader.append(instructionText);
-  $("#ingredientHeader").append(instructionHeader);
-}
-
-function getRecipe() {
-  $("#recipeSummary").empty();
-
-  var recipeUrl =
-    "https://api.spoonacular.com/recipes/random?apiKey=7c0986ad6c3445a491cf76f7d2a655ab";
-
-  $.ajax({
-    url: recipeUrl,
-    method: "GET"
-  }).then(function(recipeResponse) {
-    console.log(recipeResponse.recipes[0]);
-    var recipeImage = recipeResponse.recipes[0].image;
-    var recipeName = recipeResponse.recipes[0].title;
-    var recipeSummary = recipeResponse.recipes[0].summary;
-
-    summaryHTML = $("<p>").text(recipeSummary);
-
-    $("#recipeImage").css("background-image", "url(" + recipeImage + ")");
-    $("#recipeName").text(recipeName);
-    $("#recipeSummary").append(recipeSummary);
-
-    //render recipe history buttons
-    var newRecipe = $("<button>");
-    newRecipe.addClass("button recipe-button").text(recipeName);
-    $("#recipe-history").append(newRecipe);
-    $(".recipe-button")
-      .first()
-      .remove();
-    recipeList.shift();
-    var recipeEL = {
-      reciImg: recipeImage,
-      reciNam: recipeName,
-      reciSum: recipeSummary
-    };
-    recipeList.push(recipeEL);
-    localStorage.setItem("recipeHistory", JSON.stringify(recipeList));
-  });
-}
-
-function getMovie() {
-  $("#movieInfo").empty();
-
-  var movieUrl =
-    "https://api.themoviedb.org/3/discover/movie?api_key=f239018644aca27fb1793b1dbcab8d4c";
-
-  $.ajax({
-    url: movieUrl,
-    method: "GET"
-  }).then(function(movieResponse) {
-    var movieID = Math.floor(Math.random() * 19);
-    console.log(movieResponse.results[movieID]);
-    var movie = movieResponse.results[movieID].title;
-    var poster =
-      "https://image.tmdb.org/t/p/original" +
-      movieResponse.results[movieID].poster_path;
-    var rating = movieResponse.results[movieID].vote_average + "/10";
-    var plot = movieResponse.results[movieID].overview;
-
-    $("#movieName").text(movie);
-
-    $("#movieImage").css("background-image", "url(" + poster + ")");
-
-    ratingHeader = $("<p>").text("Rating:");
-    ratingText = $("<p>").text(rating);
-    plotHeader = $("<p>").text("Plot:");
-    plotText = $("<p>").text(plot);
-    breakHTML = $("<br>");
-
-    $("#movieInfo").append(
-      ratingHeader,
-      ratingText,
-      breakHTML,
-      plotHeader,
-      plotText
-    );
-
-    //render movie history buttons
-    var newMovie = $("<button>");
-    newMovie.addClass("button movie-button").text(movie);
-    $("#movie-history").append(newMovie);
-    $(".movie-button")
-      .first()
-      .remove();
-    movieList.shift();
-    var movieEl = {
-      movNam: movie,
-      movImg: poster,
-      movRate: rating,
-      movPlot: plot
-    };
-    movieList.push(movieEl);
-    localStorage.setItem("movieHistory", JSON.stringify(movieList));
-  });
-}
-
-function init() {
-  //keep last three random history
-  var drinkHistory = JSON.parse(localStorage.getItem("drinkHistory"));
-  if (drinkHistory == null) {
-    localStorage.setItem("drinkHistory", JSON.stringify(drinkList));
-    for (var i = 0; i < drinkList.length; i++) {
-      $("#drink-" + i).text(drinkList[i]);
-    }
-  } //handle first time use
-  drinkList = drinkHistory;
-  for (var i = 0; i < drinkHistory.length; i++) {
-    $("#drink-" + i).text(drinkHistory[i].drinkNam);
-  }
-
-  var recipeHistory = JSON.parse(localStorage.getItem("recipeHistory"));
-  if (recipeHistory == null) {
-    localStorage.setItem("recipeHistory", JSON.stringify(recipeList));
-    for (var i = 0; i < recipeList.length; i++) {
-      $("#recipe-" + i).text(recipeList[i]);
-    }
-  } //handle first time use
-  recipeList = recipeHistory;
-  for (var i = 0; i < recipeHistory.length; i++) {
-    $("#recipe-" + i).text(recipeHistory[i].reciNam);
-  }
-
-  var movHistory = JSON.parse(localStorage.getItem("movieHistory"));
-  if (movHistory == null) {
-    localStorage.setItem("movieHistory", JSON.stringify(movieList));
-    for (var i = 0; i < movieList.length; i++) {
-      $("#movie-" + i).text(movieList[i]);
-    }
-  } //handle first time use
-  movieList = movHistory;
-  for (var i = 0; i < movHistory.length; i++) {
-    $("#movie-" + i).text(movHistory[i].movNam);
-  }
-}
